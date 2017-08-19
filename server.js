36810535adaf59f5b6a931f9a426c5c2b5ff916f
schsrao <30805680+schsrao@users.noreply.github.com>
2017-08-19 20:39:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/articla-one', function (re, res) {
+   res.send('article-one'); 
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
